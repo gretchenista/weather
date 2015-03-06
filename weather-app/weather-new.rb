437c340fe4d9fb122bd9cc54cc74998d077080eb
gretchenista <gretchenista@gmail.com>
2015-03-06 16:16:40 -0500

    Update weather-new.rb

--- weather-app/weather-new.rb
+++ weather-app/weather-new.rb
@@ -46,55 +46,10 @@ def display_weather_message
         end
     end
 
-def get_weather(location)
-    client = Weatherman::Client.new
-    client.lookup_by_location(location).condition['text']
-    /Users/gretchen/Documents/weather-app/weather.rb
-end
+puts "Your zipcode please?"
+zipcode = gets
 
-def get_temp(location)
-    client = Weatherman::Client.new
-    client.lookup_by_location(location).condition['temp']
-    
-end
+temp = gets.location(zipcode)
 
-def get_city(location)
-    client = Weatherman::Client.new
-    client.lookup_by_location(location).location['city']
-    
-end
-
-get '/' do
-    "#{@message}"
-    erb :home
-end
-
-
-post '/weather' do
-	@post = params[:post]['location']
-	@weather = get_weather(@post).downcase
-    @temp = (get_temp(@post)*1.8+32).round.to_s
-    @city = get_city(@post)
-    
-	"#{@weather}"
-    
-	if (@weather == 'sunny')
-		erb:sunny
-        elsif (@weather =='cloudy')
-		erb:cloudy
-        elsif (@weather == 'partly cloudy')
-		erb :cloudy
-        elsif (@weather =='rainy')
-		erb :rainy
-        elsif (@weather =='snowy')
-		erb :snowy
-        elsif (@weather == 'foggy')
-		erb :foggy
-        elsif (@weather == 'stormy')
-        erb :stormy
-        elsif (@weather == 'severe')
-        erb :severe
-        else 
-		erb :otherweather
-	end
-end
+message = display.weather(temp)
+puts message
